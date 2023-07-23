cb6f4237aa9cd9b232e9fac22d49eb2c4f73e32f
pat chan <patc888@gmail.com>
2023-07-23 22:20:58 +0000

    updates

--- pages/Login/jsobjects/page_load/page_load.js
+++ pages/Login/jsobjects/page_load/page_load.js
@@ -1,8 +1,10 @@
 export default {
 	async checkLoggedIn () {
 		if (appsmith.URL.queryParams.code != null) {
-			storeValue('jwt', post_token.data);
+			var token = post_token.data;
+			await storeValue('jwt', token);
 			navigateTo("Stories");
 		}
+		return 200;
 	}
 }
--- pages/Stories/jsobjects/page_load/page_load.js
+++ pages/Stories/jsobjects/page_load/page_load.js
@@ -7,15 +7,20 @@ export default {
 		if (appsmith.store.jwt == null) {
 			navigateTo("Login");
 		}
-	},
-	get_stories () {
 		try {
-			return get_stories.data;
-		} catch (error) {
-			removeValue('jwt');
-			showAlert(error);
-			console.log(error);
-			navigateTo("Login");
+			console.log("getting stories");
+			var stories = get_stories.data;
+			if (stories.message == "Unauthorized") {
+				console.log("error");				
+   			navigateTo("Login");
+			}
+			console.log("got stories");
+			console.log(stories);
+		  await storeValue('stories', stories);
+		} catch (e) {
+			console.log("got error");
+			console.log(e.message);
 		}
+		return 200;
 	}
 }
