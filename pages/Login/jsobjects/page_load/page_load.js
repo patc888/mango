efdf93082b4b6977e2fa306cea31d8971af1e0d1
pat chan <patc888@gmail.com>
2023-07-24 00:23:38 +0000

    updates

--- pages/Login/jsobjects/page_load/page_load.js
+++ pages/Login/jsobjects/page_load/page_load.js
@@ -1,10 +1,19 @@
 export default {
 	async checkLoggedIn () {
-		if (appsmith.URL.queryParams.code != null) {
-			var token = post_token.data;
-			await storeValue('jwt', token);
+		console.log(appsmith.URL.queryParams);
+
+		if (appsmith.URL.queryParams.hasOwnProperty('code')) {
+			var token = post_token.run();
+			await storeValue('jwt', await token);
 			navigateTo("Stories");
+		} else {
+			console.log("else");
 		}
 		return 200;
+	},
+	async signout() {
+		await removeValue('jwt');
+		await removeValue('stories');
+		navigateTo("https://mango.auth.us-west-2.amazoncognito.com/logout?client_id=2rorg9emamtqlnnbcqh0av15bv&response_type=code&scope=email+openid&redirect_uri=https%3A%2F%2Fapp.appsmith.com%2Fapp%2Fmango%2Flogin-64b96b1831620d2266b2992f%2Fedit")
 	}
 }
--- pages/Stories/jsobjects/page_load/page_load.js
+++ pages/Stories/jsobjects/page_load/page_load.js
@@ -7,20 +7,13 @@ export default {
 		if (appsmith.store.jwt == null) {
 			navigateTo("Login");
 		}
-		try {
-			console.log("getting stories");
-			var stories = get_stories.data;
-			if (stories.message == "Unauthorized") {
-				console.log("error");				
-   			navigateTo("Login");
-			}
-			console.log("got stories");
-			console.log(stories);
-		  await storeValue('stories', stories);
-		} catch (e) {
-			console.log("got error");
-			console.log(e.message);
+		var stories = get_stories.data;
+		if (stories != null && stories.hasOwnProperty('message') && stories.message == "Unauthorized") {
+			await removeValue('stories');
+			navigateTo("Login");
+			return;
 		}
+		await storeValue('stories', stories);
 		return 200;
 	}
 }
